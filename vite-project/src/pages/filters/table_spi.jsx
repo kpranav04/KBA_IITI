b1e423c68db6ea1bffa42e89b04613794ac0fffc
Kpranav <117839743+kpranav04@users.noreply.github.com>
2024-05-21 12:13:02 +0530

    Update table_spi.jsx

--- vite-project/src/pages/filters/table_spi.jsx
+++ vite-project/src/pages/filters/table_spi.jsx
@@ -25,14 +25,14 @@ const Tabble = ({ item, index1 }) => {
     const handleSubmitButton = async (e) => {
         e.preventDefault();
         console.log(editData);
-        const result = await axios.put("http://localhost:3000/index/edit_spi", {id:item._id,data:editData});
+        const result = await axios.put("https://kba-iiti-api.vercel.app/index/edit_spi", {id:item._id,data:editData});
         setEditEnabled(!editEnabled);
          console.log(result);
         //  navigate.push('/user/profile');
     };
     const handleDelete = async (e)=>{
         e.preventDefault();
-        const result = await axios.put("http://localhost:3000/index/delete_spi", {id:item._id});
+        const result = await axios.put("https://kba-iiti-api.vercel.app/index/delete_spi", {id:item._id});
 
     }
     return (
