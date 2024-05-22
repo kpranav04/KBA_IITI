69b494d1251583ca8247d8c8ec350d3726a6c7d2
Kpranav <117839743+kpranav04@users.noreply.github.com>
2024-05-22 09:00:56 +0530

    Update table_ssi.jsx

--- vite-project/src/pages/filters/table_ssi.jsx
+++ vite-project/src/pages/filters/table_ssi.jsx
@@ -24,14 +24,14 @@ const Tabble = ({ item, index1 }) => {
     const handleSubmitButton = async (e) => {
         e.preventDefault();
         console.log(editData);
-        const result = await axios.put("https://kba-iiti-api.vercel.app/index/edit_ssi", {id:item._id,data:editData});
+        const result = await axios.put("https://kba-iiti-1.onrender.com/index/edit_ssi", {id:item._id,data:editData});
         setEditEnabled(!editEnabled);
          console.log(result);
         //  navigate.push('/user/profile');
     };
     const handleDelete = async (e)=>{
         e.preventDefault();
-        const result = await axios.put("https://kba-iiti-api.vercel.app/index/delete_ssi", {id:item._id});
+        const result = await axios.put("https://kba-iiti-1.onrender.com/index/delete_ssi", {id:item._id});
 
     }
     return (
